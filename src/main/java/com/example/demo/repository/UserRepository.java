d23ad61f80d0bbb1271355d2499b96ce4a05ebba
pa-mi-su <pamisu@protonmail.com>
2025-05-09 13:28:21 -0400

    ✅ Initial working secure microservice with JWT, Swagger, PostgreSQL, Docker

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username); // ✅ MUST return Optional
 }
